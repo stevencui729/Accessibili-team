0cd72266ed8b5902f7d688b1387ce6a4a091eab1
stevencui729 <stevencui729@gmail.com>
2019-05-27 23:40:52 -0500

    added play button to page

--- testScript.js
+++ testScript.js
@@ -1,11 +1,13 @@
 
 console.log("hello there we are in the testScript")
 console.log(document)
-console.log(chrome.runtime.getURL("testPage.html"));
+var target_url = chrome.runtime.getURL("ssl/expired.html");
+// var target_url = chrome.runtime.getURL("text_to_speech/index.html");
+console.log(target_url)
 
 // fetch(chrome.runtime.getURL("testPage.html"))
 //   .then((response) => console.log(response))
 //   .catch((err) => console.log(err))
 
 // window.location.href = chrome.runtime.getURL("testPage.html");
-window.location.replace(chrome.runtime.getURL("testPage.html"));
+window.location.replace(target_url);
