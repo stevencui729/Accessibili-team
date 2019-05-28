e726ac9409e95c6a9e0c375be83e6f5548aaf46f
stevencui729 <stevencui729@gmail.com>
2019-05-27 23:15:06 -0500

    finished with extension replacement

--- testScript.js
+++ testScript.js
@@ -1,2 +1,11 @@
 
 console.log("hello there we are in the testScript")
+console.log(document)
+console.log(chrome.runtime.getURL("testPage.html"));
+
+// fetch(chrome.runtime.getURL("testPage.html"))
+//   .then((response) => console.log(response))
+//   .catch((err) => console.log(err))
+
+// window.location.href = chrome.runtime.getURL("testPage.html");
+window.location.replace(chrome.runtime.getURL("testPage.html"));
