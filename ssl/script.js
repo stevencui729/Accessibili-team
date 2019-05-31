6ea6aae0b115f09b2598eed0500daefd07b649a2
stevencui729 <stevencui729@gmail.com>
2019-05-30 20:22:08 -0500

    audio buttons work

--- ssl/script.js
+++ ssl/script.js
@@ -3,27 +3,38 @@ var synth = window.speechSynthesis;
 //var inputForm = document.querySelector('form');
 //var voiceSelect = document.querySelector('select');
 
+const text = 'The site is not secure. Click the blue button to return to safety' 
+let utterThis = new SpeechSynthesisUtterance(text);
+utterThis.pitch = 1;
+utterThis.rate = 1;
+console.log(utterThis)
 
-function speak(text){
-    if (synth.speaking) {
-        console.error('speechSynthesis.speaking');
-        return;
-    }
-    let  utterThis = new SpeechSynthesisUtterance(text);
-    utterThis.onend = function (event) {
-        console.log('SpeechSynthesisUtterance.onend');
-    }
-    utterThis.onerror = function (event) {
-        console.error('SpeechSynthesisUtterance.onerror');
-    }
-    //utterThis.pitch = 1;
-    //utterThis.rate = 1;
-    synth.speak(utterThis);
+// function speak(text){
+//   if (synth.speaking) {
+//     console.error('speechSynthesis.speaking');
+//     return;
+//   }
+  
+//   utterThis.onend = function (event) {
+//     console.log('SpeechSynthesisUtterance.onend');
+//   }
+//   utterThis.onerror = function (event) {
+//     console.error('SpeechSynthesisUtterance.onerror');
+//   }
+//   //utterThis.pitch = 1;
+//   //utterThis.rate = 1;
+
+// }
+synth.speak(utterThis);
+if (synth.speaking) {
+  console.log('speechSynthesis.speaking');
 }
 
-onsubmit = function(event) {
-  event.preventDefault();
+document.getElementById("playButton").addEventListener("click", function() {
+  synth.speak(utterThis);
+  
+})
 
-  const text = 'The site is not secure. Click the blue button to return to safety' 
-  speak(text);
-}
+document.getElementById("pauseButton").addEventListener("click", function() {
+  synth.pause(utterThis);
+})
